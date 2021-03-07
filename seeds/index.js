db3cb81b8be695a63e96b94a4d5fdb52f6085d41
Kulbhushan goswami <igoswamik457@gmail.com>
2021-03-07 17:26:09 +0530

    Added Basic Styles

--- seeds/index.js
+++ seeds/index.js
@@ -16,14 +16,18 @@ db.once('open', ()=>{
 });
 
 const sample=array=> array[Math.floor(Math.random()*array.length)];
-
+// 'https://source.unsplash.com/collection/483251'  //we will get different image everytime using this API
 const seedDB=async()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
         const camp=new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)}, ${sample(places)}`
+            title: `${sample(descriptors)}, ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ipsum incidunt, repellendus corporis corrupti harum eum cum recusandae, eveniet distinctio a, saepe voluptatibus! Repudiandae, eos! Ea aliquid iure nihil id?',
+            price
         })
         await camp.save();
     }
