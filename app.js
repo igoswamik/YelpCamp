e094f539a00b354619ed790cc665e893180f2579
Kulbhushan goswami <igoswamik457@gmail.com>
2021-04-07 19:19:34 +0530

    add start script

--- app.js
+++ app.js
@@ -182,6 +182,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log("SERVING ON PORT 3000!");
+const port=process.env.PORT ||3000 ; //process.env.PORT will be automatically present on heroku
+app.listen(port,()=>{
+    console.log(`SERVING ON PORT ${port}!`);
 })
